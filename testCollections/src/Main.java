d2bb6f955eb81a175bc73b054243f14f946bd7f3
joaonunes833 <joaonunes833@hotmail.com>
2022-11-03 10:56:58 +0000

    03/11

---
+++ testCollections/src/Main.java
@@ -0,0 +1,36 @@
+import testCollections.Point.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Point> arrayList= new ArrayList<Point>();
+
+        arrayList.add(new Point());
+        arrayList.add(new Point(10,10));
+        arrayList.add(new Point(11,11));
+        arrayList.add(new Point(12,12));
+
+        Point element = arrayList.get(2);
+
+        arrayList.remove(0);
+
+        for(int i = 0;i < arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Hello");
+        String str = stack.pop();
+
+        HashMap<String, Point> map = new HashMap<String, Point>();
+
+        map.put("Point1", new Point());
+        map.put("Point2", new Point(10,10));
+
+        Point pm = map.get("Point2");
+
+    }
+}
---
+++ testCollections/src/testCollections/Point/Point.java
@@ -0,0 +1,52 @@
+package testCollections.Point;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point() {
+        x = 0.0;
+        y = 0.0;
+    }
+    public Point(final double x,
+                 final double y) {
+        this.y = y;
+        this.x = x;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    public void setX(int value) {
+        x = value;
+    }
+    public void setY(int value) {
+        y = value;
+    }
+
+    public double distanceTo(Point other){
+        double distance = 0;
+        distance = Math.sqrt(Math.pow(other.x - this.x,2)+Math.pow(other.x - this.x,2));
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Point other = (Point) o;
+        if (this.x == other.x && this.y == other.y)
+            return true;
+        else
+            return false;
+    }
+}
